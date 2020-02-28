eeb1a54f748d7233594502d73ad2fba7e30deffd
Yordan Radev <asofficialasitgets@gmail.com>
2020-02-28 10:03:01 -0500

    Some structure added based on the Assignment document

--- kernel.c
+++ kernel.c
@@ -1,1 +1,17 @@
 This is where the actual main method would be
+
+myinit(char *filename)
+{
+	addToRAM(FILE *p, int *start, int *end);
+	makePCB(int start , int end);
+	addToReady(PCB *)
+
+
+}
+
+scheduler()
+{
+	isCPUAvailable();
+
+
+}
--- ram.c
+++ ram.c
@@ -1,1 +1,31 @@
 an array of size 3 to hold the up to 3 programs
+
+char *ram[1000];
+
+loadToRam
+{
+	int startInRam = base;
+	int counteroflines = 0;
+	int endInRam;
+	fopen(script)
+	getNextCellRam()
+	copyLinesToRam()
+	endInRam= startInRam + counteroflines;
+
+	helper
+	{
+		fgets(file,buffer,limit)
+		ram[k] = strdup(buffer)
+	}
+
+	clearRam
+	{
+		for ( i=0 ; i < 1000 ; i++ )
+		{
+			ram[i] = NULL;
+		}
+	}
+
+
+
+}
